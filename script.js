3c5623c2da4213def63eb3babb07af83c4063d76
yashaswinitula <ttulayashaswini@gmail.com>
2025-12-23 11:51:51 +0530

    Add Task 01 landing page files

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
